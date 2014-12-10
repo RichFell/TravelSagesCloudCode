2af3d190ec8fc9e077874fab99039b02b214407f
Rich Fellure <rfellure24@gmail.com>
2014-12-09 21:30:55 -0600

    New delete method

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,22 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.afterDelete("Region", function(request) {
+	var query = new Parse.Query("Location");
+	query.equalTo("region", request);
+	query.find({
+		success: function(locations) {
+			Parse.Object.destroyAll(locations, {
+				success: function() {
+					console.log("Destroyed all locations for Region");
+				},
+				error: function(error) {
+					console.error("Error deleting locations" + error.code);
+				}
+			});
+		},
+		error: function(error) {
+			console.error("Error finding locations" + error.code);
+		}
+	}); 
 });
